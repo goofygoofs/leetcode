47c28dedef9130b70888b0c8a0e957e4df23e70b
goofy <goofydinh@gmail.com>
2022-03-29 20:05:10 -0700

    added first attempt at num of good ways to split a string

--- number of good ways to split a string/index.py
+++ number of good ways to split a string/index.py
@@ -1,7 +1,8 @@
 '''
 You are given a string s.
 
-A split is called good if you can split s into two non-empty strings sleft and sright where their concatenation is equal to s (i.e., sleft + sright = s) and the number of distinct letters in sleft and sright is the same.
+A split is called good if you can split s into two non-empty strings sleft and sright where their concatenation is equal to s (i.e., sleft + sright = s) 
+and the number of distinct letters in sleft and sright is the same.
 
 Return the number of good splits you can make in s.
 
@@ -31,8 +32,49 @@ s consists of only lowercase English letters.
 '''
 
 '''
+26 letters in alphabet
+set / dict to find unique
+ 
+ go through it n times, and at each spot, either return True if good split or just add one as we are traversing through n
 
+set for left side, dict for right side, so we don't have to keep looping 
 '''
 class Solution:
     def numSplits(self, s: str) -> int:
+        goodSplits = 0
+        leftSet = set()
+        rightDict = dict()
+        # populate rightDict
+        for char in s:
+            if char not in rightDict:
+                rightDict[char] = 1
+            else:
+                rightDict[char] += 1
         
+        uniqueLeft = 0
+        uniqueRight = len(rightDict) 
+        for char in s:
+            if char not in leftSet:
+                leftSet.add(char)
+                uniqueLeft += 1
+            rightDict[char] -= 1
+            if rightDict[char] == 0:
+                del rightDict[char]
+                uniqueRight -= 1
+            # see if good split
+            if uniqueLeft == uniqueRight:
+                goodSplits += 1
+        
+        return goodSplits
+"""
+O(n) time complexity
+O(n) space
+"""
+
+sol = Solution()
+
+output = sol.numSplits("aacaba")
+print(output)
+
+output = sol.numSplits("abcde")
+print(output)
