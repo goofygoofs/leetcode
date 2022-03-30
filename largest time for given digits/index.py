9acbdeecd014ad6dbadb151d4e8a9c703193adbf
goofy <goofydinh@gmail.com>
2022-03-29 17:27:48 -0700

    added first attempt of solution for largest time given digits

--- largest time for given digits/index.py
+++ largest time for given digits/index.py
@@ -23,7 +23,115 @@ Constraints:
 
 arr.length == 4
 0 <= arr[i] <= 9
+
 '''
+'''
+brute method way is doing 4 for loops
+which isnt too bad because it only runs O(1) time 24 times
+highest variable which is 2359.  
+
+'''
+
+'''
+beautify?
+maybe make time property a class and update it's properties if the length of output isnt 5
+we run maximum of 3 time if failing. with first index being max number of 2, 1, 0. 
+make code into a function for re-calling if failed.
+'''
+from typing import List
 class Solution:
     def largestTimeFromDigits(self, arr: List[int]) -> str:
+        timeProperties = {
+                            (0, 2): "",
+                            (1, 9): "",
+                            (2, 0): ":",
+                            (3, 5): "",
+                            (4, 9): ""
+                        }
+                        # (9, 1) can become (3, 1) max num 3 if (2,0) has a number
+        latestTime = ""
+        # sort 
+        sortedNums = sorted(arr, reverse=True) # [1,2,3,4]
+        # update time properties if 1 or 2 exists
+        if  2 in sortedNums:
+            timeProperties = {
+                            (0, 2): "",
+                            (1, 3): "",
+                            (2, 0): ":",
+                            (3, 5): "",
+                            (4, 9): ""
+                        }
+        for num in sortedNums:
+            for property in timeProperties: # property = (#, #)
+                if num <= property[1] and len(timeProperties[property]) == 0:
+                    timeProperties[property] += str(num)
+                    break
         
+
+        for property in timeProperties:
+            latestTime += timeProperties[property]
+
+
+        if len(latestTime) < 5:
+            latestTime = ""
+            timeProperties = {
+                            (0, 1): "",
+                            (1, 9): "",
+                            (2, 0): ":",
+                            (3, 5): "",
+                            (4, 9): ""
+                        }
+            for num in sortedNums:
+                for property in timeProperties: # property = (#, #)
+                    if num <= property[1] and len(timeProperties[property]) == 0:
+                        timeProperties[property] += str(num)
+                        break
+            for property in timeProperties:
+                latestTime += timeProperties[property]
+
+            if len(latestTime) < 5:
+                latestTime = ""
+                timeProperties = {
+                                (0, 0): "",
+                                (1, 9): "",
+                                (2, 0): ":",
+                                (3, 5): "",
+                                (4, 9): ""
+                            }
+                for num in sortedNums:
+                    for property in timeProperties: # property = (#, #)
+                        if num <= property[1] and len(timeProperties[property]) == 0:
+                            timeProperties[property] += str(num)
+                            break
+                for property in timeProperties:
+                    latestTime += timeProperties[property]
+                if len(latestTime) < 5:
+                    return ""
+                return latestTime
+
+            return latestTime
+        return latestTime
+
+sol = Solution()
+
+output = sol.largestTimeFromDigits([1,2,3,4])
+print(output)
+
+output = sol.largestTimeFromDigits([0,5,5,5])
+print(output)
+
+output = sol.largestTimeFromDigits([2,0,6,6])
+print(output)
+
+output = sol.largestTimeFromDigits([2,9,1,8])
+print(output)
+
+
+
+
+
+
+
+
+
+
