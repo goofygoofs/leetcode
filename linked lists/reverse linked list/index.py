aa90585ac483421ced72cf24446893421199b055
goofy <goofydinh@gmail.com>
2022-04-06 13:55:31 -0700

    added reverse linked list

--- linked lists/reverse linked list/index.py
+++ linked lists/reverse linked list/index.py
@@ -33,10 +33,14 @@ Follow up: A linked list can be reversed either iteratively or recursively. Coul
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
