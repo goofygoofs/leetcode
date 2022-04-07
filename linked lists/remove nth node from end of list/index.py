8cb3adb6cb5abae49137a5259381ab9cb0d7300b
goofy <goofydinh@gmail.com>
2022-04-06 19:30:59 -0700

    added remove nth node from end of list

--- linked lists/remove nth node from end of list/index.py
+++ linked lists/remove nth node from end of list/index.py
@@ -30,14 +30,46 @@ Follow up: Could you do this in one pass?
 '''
 
 '''
+2 pointers: start and end
+initially create a distance n between start and end
 
+while loop until end reaches the end
+start.next = start.next.next
+return head
+
+end cases for 1 element and 2 elements in list
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        start = head
+        end = head
+
+        for _ in range(n):
+            end = end.next
         
+        # n = length of list
+        if end is None:
+            # 2 items in list
+            if start.next:
+                return head.next
+            # return None if remove only item in list
+            else:
+                return None
+        
+        while end.next:
+            end = end.next
+            start = start.next
+        
+        # at the end now remove nth node
+        start.next = start.next.next
+        return head
