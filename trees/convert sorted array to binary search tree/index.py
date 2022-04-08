c926af04c1b736e085ff88ce84fc2c2ee7727ce7
goofy <goofydinh@gmail.com>
2022-04-07 21:28:10 -0700

    added failed attempt at binary search tree, just comments

--- trees/convert sorted array to binary search tree/index.py
+++ trees/convert sorted array to binary search tree/index.py
@@ -28,15 +28,20 @@ nums is sorted in a strictly increasing order.
 '''
 
 '''
+mid index create a node out of it
 
+create function that takes in a TreeNode, the list?, and index?
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         
