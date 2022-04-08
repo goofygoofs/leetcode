fe9481a5906253a172157a4403391dcf7eaf1789
goofy <goofydinh@gmail.com>
2022-04-07 17:36:03 -0700

    updated palindrome linked list ot use fast and slow pointer

--- linked lists/palindrome linked list/index.py
+++ linked lists/palindrome linked list/index.py
@@ -35,51 +35,66 @@ iterate until pointers cross. if values not equal return False, else return True
 need a index variable to keep count
 '''
 
+'''
+there's a technique: two pointers
+one pointer is going to be fast
+one pointer is going to be slow
+why are we doing it like this?
+the main idea is if both pointers start at the beginning and let's say our slow pointer moves one spot ahead
+and our fast pointer moves two spots ahead
+then when that's the case when our fast pointer reaches the end of the list
+our slow pointer should be somewhere near the middle right
+and so this is the idea we already know 
+how to get to the beginning of the list right
+we can basically check the beginning and the end and keep doing that and check if it's a palindrome
+the only problem to these pointers is that they only go in one direction.
+there's an algo to reverse a linked list
+and that's exactly what we can do
+starting from the middle, we can reverse the linked list
+then it's easy to check if it's a palindrome or not
+again we can do this in constant memory
+we do have to update our data structure though
+but we don't need an extra array or anything
+'''
+
 from typing import Optional
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None, prev=None):
         self.val = val
         self.next = next
-        self.prev = prev
 
 
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        linkedlist = self.createListNode([1,2,2])
-        left = head
-        right = head
-        index = 0
+        fast = head
+        slow = head
 
-        # iterate right until the end
-        while right.next:
-            prev = right
-            right = right.next
-            right.prev = prev
-            index += 1
+        # keep going until fast is at the last node or reach None
+        # find middle (slow)
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
         
-        while index > 0: # 1 -> 2 -> 2 -> 1
-            # print('index', index)
+        # reverse second half
+        prev = None
+        while slow:
+            next = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next
+        
+        # check palindrome
+        left, right = head, prev
+        while right:
             if left.val != right.val:
                 return False
             left = left.next
-            right = right.prev
-            index -= 2
-        
+            right = right.next
         return True
-
-    def createListNode(self, arr: list[int]) -> Optional[ListNode]:
-        head = ListNode(arr[0])
-        curr = head
-
-        for i in range(1, len(arr)):
-            node = ListNode(arr[i])
-            prev = curr
-            curr.next = node
-            curr = node
-            curr.prev = prev
         
-        return head
+
+
 
 # sol = Solution()
 
