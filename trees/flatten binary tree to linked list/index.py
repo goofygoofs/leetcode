f1698c87ed14bb97c1dc17b6aef8064e4b45bbfb
goofy <goofydinh@gmail.com>
2022-04-10 10:44:38 -0700

    flatten binary tree to linked list

--- trees/flatten binary tree to linked list/index.py
+++ trees/flatten binary tree to linked list/index.py
@@ -30,18 +30,57 @@ Follow up: Can you flatten the tree in-place (with O(1) extra space)?
 '''
 
 '''
+preorder -> node -> left , then right
+change to linked list
 
+if node:
+    linkedlist -> node.val
+    recursive(left)
+    recursive(right)
+left pointer always None, right child is the next node in the list
+O(1) space
+
+if node:
+    left = node.left
+    right = node.right
+    node.right = left
+    node.left = None
+
+O(n) time where n is number of nodes
+O(n) space to store right subtree in stack
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
-        """
+        """ 
+        # helper will take in the next node and return the stored 
+        # right node in a stack
+        # O(m) space where m is number of nodes on right side 
+        def helper(node: Optional[TreeNode], stack: List[TreeNode]) -> None:
+            if node is None:
+                return
+            left = node.left
+            right = node.right
+            node.right = left
+            node.left = None
+            if right:
+                stack.append(right)
+            helper(node.right, stack)
+            if len(stack) > 0:
+                node.right = stack.pop()
+                helper(node.right, stack)
+            
+            return
+        helper(root, [])
         
