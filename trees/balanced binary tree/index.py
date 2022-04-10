1655f2bbcbd4d65dfa7b10756a0d3a5588736b5e
goofy <goofydinh@gmail.com>
2022-04-09 21:14:06 -0700

    added first attempt at balanced binary tree

--- trees/balanced binary tree/index.py
+++ trees/balanced binary tree/index.py
@@ -30,15 +30,51 @@ The number of nodes in the tree is in the range [0, 5000].
 '''
 
 '''
-
+recursive return left == right
+each time go down left and rigth, add 1 for each layer
+can't be more difference by 1
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
         
+
+        def helper(root: Optional[TreeNode]) -> int:
+            if root is None:
+                return 0
+            left = helper(root.left)
+            right = helper(root.right)
+            return max(1 + left, 1 + right)
+        
+        left = helper(root.left)
+        right = helper(root.right)
+
+        return left == right or left + 1 == right or left == right + 1
+
+
+tree = TreeNode(1)
+tree.left = TreeNode(2)
+tree.right = TreeNode(2)
+left = tree.left
+left.left = TreeNode(3)
+right = tree.right
+right.right = TreeNode(3)
+left = tree.left
+left.left = TreeNode(4)
+right = tree.right
+right.right = TreeNode(4)
+
+sol = Solution()
+output = sol.isBalanced(tree)
+print(output)
