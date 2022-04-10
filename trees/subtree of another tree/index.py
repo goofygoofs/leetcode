c0e4998abef8ae449079bdb918603e5a3b5db039
goofy <goofydinh@gmail.com>
2022-04-09 22:39:29 -0700

    added subtree of another tree

--- trees/subtree of another tree/index.py
+++ trees/subtree of another tree/index.py
@@ -26,15 +26,49 @@ The number of nodes in the subRoot tree is in the range [1, 1000].
 '''
 
 '''
+subtree does not return true if you compare, gotta compare the values
+if we see a matching value, we recursion through all the nodes to compare. anything that doesn't match
+break out
 
+time - O(n * m) have to go through subroot (m) if intially value is same
+space - O(m) space for stack
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        
+
+        def helper(node: Optional[TreeNode], subNode: Optional[TreeNode]) -> bool:
+            if node is None and subNode is None:
+                return True
+            if node is None:
+                return False
+            if subNode is None:
+                return False
+            if node.val != subNode.val:
+                return False
+            return helper(node.left, subNode.left) and helper(node.right, subNode.right)
+
+        queue = deque([root])
+        while len(queue) > 0:
+            node = queue.popleft()
+            left, right = node.left, node.right
+            if node.val == subRoot.val:
+                if helper(left, subRoot.left) and helper(right, subRoot.right):
+                    return True
+            if left:
+                queue.append(left)
+            if right:
+                queue.append(right)
+ 
+        return False
+
+
