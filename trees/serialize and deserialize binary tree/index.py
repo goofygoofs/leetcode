a59abd033970d67c402bdba35e9858d376d2a017
goofy <goofydinh@gmail.com>
2022-04-12 16:14:31 -0700

    added first failed attempt

--- trees/serialize and deserialize binary tree/index.py
+++ trees/serialize and deserialize binary tree/index.py
@@ -25,16 +25,23 @@ The number of nodes in the tree is in the range [0, 104].
 '''
 
 '''
-
+we are given 2 binary trees and we want to write 2 functinos for it
+serialize aand deserialize
+serialize we're taking an object in memory can be anything this node can be anywhere there's links connecting them anywhere
+put it in an easy to read string
+convert this entire tree into a string
+and we want to take a string like this and turn it into a tree
+can use BFS taking it level by level serializing the tree
+check for 'None' string in deserializing
 '''
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+from collections import deque
 class Codec:
 
     def serialize(self, root):
@@ -43,7 +50,30 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        
+        if not root:
+            return ""
+        queue = deque([root])
+        output = ""
+
+        while len(queue) > 0:
+            nextLevel = deque()
+            while len(queue) > 0:
+                node = queue.popleft()
+                if not node:
+                    output += "None,"
+                else:
+                    output += str(node.val) + ","
+                    if node.left:
+                        nextLevel.append(node.left)
+                    else:
+                        nextLevel.append(None)
+                    if node.right:
+                        nextLevel.append(node.right)
+                    else:
+                        nextLevel.append(None)
+            if len(nextLevel) > 0:
+                queue = nextLevel
+        return output[:-1]
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -51,6 +81,45 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        if not data:
+            return None
+        # [1,2,3,null,null,4,5]
+        # [2,3,null,null,4,5]
+        # [null,null,4,5]
+        queue = deque(data.split(','))
+        root = TreeNode(queue.popleft())
+        # []
+        treeQueue = deque([root])
+        
+        # left 2
+        # right 3
+
+        while len(queue) > 0:
+            nextLevel = deque()
+            while len(treeQueue) > 0:
+                node = treeQueue.popleft()
+                left, right = queue.popleft(), queue.popleft()
+                if left != 'None':
+                    left = TreeNode(left)
+                else:
+                    left = None
+                node.left = left
+                if right != 'None':
+                    right = TreeNode(right)
+                else:
+                    right = None
+                node.right = right
+                # check if add to next level
+                if left != None:
+                    nextLevel.append(left)
+                if right != None:
+                    nextLevel.append(right)
+            if len(nextLevel) > 0:
+                treeQueue = nextLevel
+        return root
+            
+
+
         
 
 # Your Codec object will be instantiated and called as such:
