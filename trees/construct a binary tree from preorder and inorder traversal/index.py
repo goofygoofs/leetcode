eafb7a1317867968cb5f551f199a77f383567eed
goofy <goofydinh@gmail.com>
2022-04-11 21:54:31 -0700

    added first failed attempt at construct a binary tree from pre-in order

--- trees/construct a binary tree from preorder and inorder traversal/index.py
+++ trees/construct a binary tree from preorder and inorder traversal/index.py
@@ -26,15 +26,49 @@ inorder is guaranteed to be the inorder traversal of the tree.
 '''
 
 '''
-
+pre-order - will keep going left, (node unless already visited), then right
+in-order will start at the left most, the local node, the right, go up to the next node, then right
+get the root from the pre-order first
+keep going in pre-order until it hits first of in order (left most root)
+then get the right from in-order
+use queue on both
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List, Optional
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        preorder = deque(preorder)
+        inorder = deque(inorder)
+        root = TreeNode(preorder.popleft())
+        curr = root
+
+        # remove first inorder if equal to preorder root
+        if root.val == inorder[0]:
+            inorder.popleft()
         
+        def helper(node: Optional[TreeNode]) -> None:
+            if len(preorder) > 0 and len(inorder) > 0:
+                left = TreeNode(preorder.popleft())
+                node.left = left
+                # can still keep going left until leftmost leaf
+                if left.val != inorder[0]:
+                    helper(node.left)
+                else:
+                    while inorder[0] != node.val:
+                        inorder.popleft()
+                    inorder.popleft()
+                    preorder.popleft()
+                right = TreeNode(inorder.popleft())
+                node.right = right
+
+        if len(preorder) > 0:
+            helper(curr)
+        return root
