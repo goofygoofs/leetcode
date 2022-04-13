082b0b97ae49d4dc5cfd9671e859629cfe410675
goofy <goofydinh@gmail.com>
2022-04-13 16:42:42 -0700

    added all paths from source to target

--- graphs/all paths from source to target/index.py
+++ graphs/all paths from source to target/index.py
@@ -29,9 +29,63 @@ The input graph is guaranteed to be a DAG.
 '''
 
 '''
+nodes are labeled from 0 to n - 1. (increasing order from 0... n-1)
+dfs with backtracking and global output set. so we try to get to n-1,
+we dfs and have a "banned" set that we add to and remove from once we visited
+graph basically gives us neighbors to go through
+let's first make a dictionary with
+{
+    node: set(neighbors)
+}
 
+# make a copy of the array so it doesn't change by reference
+set does not preserve order so need to have a set for faster access AND an array to preserve order
+copy the array and add it to output so it doesn't change by reference
+
+time - O(n^2?) or O(n)
+space O(n) for set, and array
 '''
+
+from typing import List, Dict, OrderedDict, Set, Tuple
+from collections import defaultdict
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        dict = defaultdict(set)       
+        output = (len(graph) - 1, []) # n-1 and answer
+        # use a set for faster access and list to preserve order
+        visited = (set(), [])
+        for i in range(len(graph)):
+            # node: set(neighbors)
+            for neighbor in graph[i]:
+                dict[i].add(neighbor)
+
+        # print('dict', dict)
         
+
+        def helper(cur: int, dict: Dict[int, Set[int]], visited: Tuple[Set[int], List[int]], output: Tuple[int, List[List[int]]]) -> None:
+            if cur == output[0]:
+                # make a copy of the array so it doesn't change by reference
+                copyVisited = visited[1].copy()
+                output[1].append(copyVisited)
+                # assuming n-1 doesn't direct to anywhere else
+                return
+            for neighbor in dict[cur]:
+                if neighbor not in visited[0]:
+                    visited[0].add(neighbor), visited[1].append(neighbor)
+                    
+
+                    # print('cur', cur, 'neighbor', neighbor)
+
+                    helper(neighbor, dict, visited, output)
+                    visited[0].remove(neighbor), visited[1].remove(neighbor)
+            
+        
+        visited[0].add(0), visited[1].append(0)
+        helper(0, dict, visited, output)
+        return output[1]
+
+
+# sol = Solution()
+# output = sol.allPathsSourceTarget([[4,3,1],[3,2,4],[3],[4],[]])
+# print(output)
