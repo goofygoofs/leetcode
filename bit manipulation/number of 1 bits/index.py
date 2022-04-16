e8433c89f5864dff008e783a4d55a38b5fb2cd33
goofy <goofydinh@gmail.com>
2022-04-15 17:31:15 -0700

    completed num of 1 bits by modding by 2 and shifting bit to the right by 1

--- bit manipulation/number of 1 bits/index.py
+++ bit manipulation/number of 1 bits/index.py
@@ -33,14 +33,52 @@ Follow up: If this function is called many times, how would you optimize it?
 '''
 
 '''
+take a look at this number and return the number of 1's it has
+2 solutions
+count manually bit by bit.
+look at the bit on the right side. logic and operation it's gonna be a 0 or 1
+for every bit. it's gonna be 0 for every bit except for the rightmost one which can be 0 or 1
+it'll be 1 if the bit in the input integer is 1 then we'll get a 1 in the output if it's a 0
 
+we can mod it by % 2 and we'll get a 1 if the remainder is 1 or a 0 if the remainder is 0 (the last integer)
+so we have a way to look at the last bit and see if it's 1 or 0.
+how do we check the other bits?
+we can take all the rest of the bits and then shift them to the right by one
+shift every bit to the right by one
+dividing by /2 will usually shift all the bits to the right by one but usually the bit shift operation
+is a little bit more efficient on your cpu so that's what we're gonna prefer
+we're gonna take the bits and shift over the right by 1 then mod % 2 to find out if 0 or 1
+once we have the last 1 and shift it to the right, we only gonna have a 0, so that's how we know we're done
+
+guaranteed that every input is going to be a 32-bit integer so the while loop we had is going to run 32 times
+so time complexity is 32 so constant
+time complexity O(32) => O(1)
+space O(1)
+downside to our solution is that is looks at every bit even if it isn't 1
+wouldn't it be convenient if our algo only runs when there's a 1?
 '''
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        count1 = 0
-        strN = str(n)
-        for i in range(len(strN)):
-            if strN[i] == '1':
-                count1 += 1
-        return count1
+        res = 0
+        while n:
+            # %2 will return 0 if 0 and 1 if 1 for the rightmost bit
+            res += n % 2
+            # shift bit to the right by 1
+            n = n >> 1
+        return res
+
+        # 2nd solution. runs only for how many 1's there is
+        # why does this work?
+        # we subtract 1 it removes the last 1 bit and the & and will zero out everything but remaining 1's
+        # we then increment for each time this happens
+        # basically what we're doing is skipping all the 0's in between
+        # and we're running this algo as many times as there are 1's in this input integer
+        n = n & (n-1)
+        res += 1
+
+        res = 0
+        while n:
+            n &= (n-1)
+            res += 1
+        return res
