33fcf149478866cf73e8e2d1bcf14c346263a684
goofy <goofydinh@gmail.com>
2022-04-19 13:52:48 -0700

    failed at finidng O(1) sol to multiply strings

--- math and geometry/multiply strings/index.py
+++ math and geometry/multiply strings/index.py
@@ -22,6 +22,55 @@ num1 and num2 consist of digits only.
 Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 '''
 
+'''
+"2" * "3"
+we cannot use int()
+
+dict "0-9": 0-9
+for loop through the str?
+[1,2,3],join()
+
+O(n) time n len(num1) + len(num2)
+O(n) for the dictionary, array
+
+O(1) dict
+
+how do we do multiplication
+
+    123
+x   456
+--------
+    738
+   6150 
+  49200
+--------
+  56088
+0) for loop within for loop
+1) right to left - pointer
+2) carry overs
+-----------
+ 1) add right to left - poitner
+ 2) carry over
+'''
+
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        
+        dict = {
+            "0": 0,
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9
+        }
+
+        for i in range(-1, len(num1), -1):
+            pass
+
+print('hello')
+for i in range(3, 1,-1):
+    print(i)
