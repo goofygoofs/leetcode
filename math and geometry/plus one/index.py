4fa54730e6aae19937b56788f90a875501f50296
goofy <goofydinh@gmail.com>
2022-04-19 11:14:03 -0700

    completed plus One using pointer for O(1) space

--- math and geometry/plus one/index.py
+++ math and geometry/plus one/index.py
@@ -35,9 +35,45 @@ Constraints:
 digits does not contain any leading 0's.
 '''
 
+'''
+array problem
+obvious -> loop through and "" + str(int)
+int(str) + 1 => array
+O(n) time
+O(n) space - str buildup
+
+array - properties 
+only time make array bigger is if we have a 9 specifically 9 in the last digits, this can also affect if there's 
+subsequent 9's before that 9
+
+besides the 9
+we can alter the original array last digit by +1, and return it
+
+what if it's a 9?
+1) a bigger array by 1 digit ie) 9=>10 => O(n) [1] + original array
+2) 89 => 90. while loop that we reach a digit not 9, just increment by 1, if it's a 9, change it to a 0
+
+'''
+
 from typing import List
 
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
+        pointer = len(digits) - 1
+
+        digits[pointer] += 1
+        extraDigit = False
+
+        while digits[pointer] == 10:
+            digits[pointer] = 0
+            if pointer == 0 and digits[pointer] == 0:
+                extraDigit = True
+            pointer -= 1
+            if pointer >= 0:
+                digits[pointer] += 1
+
+        if extraDigit:
+            return [1] + digits
+        return digits
+             
