e7cc64d514369c3b41f98a431358094145c8a3c6
goofy <goofydinh@gmail.com>
2022-04-19 14:24:08 -0700

    added multiply strings

--- math and geometry/multiply strings/index.py
+++ math and geometry/multiply strings/index.py
@@ -53,24 +53,40 @@ x   456
  2) carry over
 '''
 
+'''
+create with an array and convert back to a string afterwards
+time - O(m*n) m is len(num1) and n len(num2)
+memory - O(m+n) for the array
+
+gotta do it in reverse (9*9)
+remove beginning zero's
+'''
+
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        dict = {
-            "0": 0,
-            "1": 1,
-            "2": 2,
-            "3": 3,
-            "4": 4,
-            "5": 5,
-            "6": 6,
-            "7": 7,
-            "8": 8,
-            "9": 9
-        }
+        if "0" in [num1, num2]:
+            return "0"
+        
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        print(res)
 
-        for i in range(-1, len(num1), -1):
-            pass
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                digit = int(num1[i]) * int(num2[j])
+                print('i+j', i+j, i, j)
+                res[i + j] += digit
+                res[i + j + 1] += res[i + j] // 10 # for carry over
+                res[i + j] = res[i + j] % 10 # remainder
+                print(res)
 
-print('hello')
-for i in range(3, 1,-1):
-    print(i)
+        # get rid of any leading 0's [5,6,0,8,8,0]
+        res, beg = res[::-1], 0
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+        res = map(str, res[beg:])
+        return "".join(res)
+
+sol = Solution()
+output = sol.multiply("9", "9")
+print(output)
