ad42e65448e95efcc76f7e618e78644a2fd885da
goofy <goofydinh@gmail.com>
2022-04-19 18:04:57 -0700

    added solution to climbing stairs without reasoning

--- 1-D dynamic programming/climbing stairs/index.py
+++ 1-D dynamic programming/climbing stairs/index.py
@@ -28,9 +28,56 @@ Constraints:
 '''
 
 '''
+3
+if it's the same number, order does not matter
+different numbers, order matters
+1 + 1 + 1
+1 + 2
+2 + 1
+
+1 - 1
+2 - 2
+
+4
+2(3) + 2(3) = 6
+1 + 1 + 1 + 1
+2 + 2
+1 + 2 + 1
+1 + 1 + 2
+2 + 1 + 1
+
+_ _ _ _ _ _  _
+1 2 3 5 8 13 21
+
+pattern we get the prev + prev.prev amount of ways will give us current
+but question is why?
+NOT SURE 
+top->bottom => because we need the previous 2 to compute the next one
+store the last 2 we've seen
+
+time - O(0->n) range
+space - O(1)
+
+reasoning ?
+'''
+
+'''
 
 '''
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        prev = 1
+        prevPrev = 0
+        index = 0
+
+        if n == 1:
+            return 1
+
+        while index < n:
+            index += 1
+            tmp = prevPrev
+            prevPrev = prev
+            prev += tmp
+        return prev
+
