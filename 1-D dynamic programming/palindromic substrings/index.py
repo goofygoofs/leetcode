f92efb2368ca67d74013f387bea3350beeecf2df
goofy <goofydinh@gmail.com>
2022-04-22 10:53:26 -0700

    add palindromic substrings

--- 1-D dynamic programming/palindromic substrings/index.py
+++ 1-D dynamic programming/palindromic substrings/index.py
@@ -26,9 +26,37 @@ s consists of lowercase English letters.
 '''
 
 '''
+a and a at diff index do count
+we expand out from the middle character => O(n^2)
+check even and odd edge case
+set() stores (pal substring, startingIndex) => store duplicates at diff index
 
+time - O(n^2)
+space - O(n) 
+
+update: remove set and just update count for O(1) space
 '''
 
+# O(1) space
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        
+        self.count = 0
+        def helper(i: int) -> None: # find all palindromes at given index expanded outwards. i is middle of palindrome
+            l = i-1
+            r = i+1
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                self.count += 1
+                l-=1
+                r+=1
+            # even case
+            l = i
+            r = i+1
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                self.count += 1
+                l-=1
+                r+=1
+        for i in range(len(s)):
+            self.count +=1
+            helper(i)
+        return self.count
+
