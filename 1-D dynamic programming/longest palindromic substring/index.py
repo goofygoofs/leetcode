fccc254f57b0ea8d822be5b7b6dd1be34df7d5f2
goofy <goofydinh@gmail.com>
2022-04-22 10:38:02 -0700

    added longest pal strin

--- 1-D dynamic programming/longest palindromic substring/index.py
+++ 1-D dynamic programming/longest palindromic substring/index.py
@@ -21,9 +21,63 @@ s consist of only digits and English letters.
 '''
 
 '''
+palindrome? 
+spelled same backward as forward
+bab
+pointers
+left, right
+zbabad
+l    r
+Q: how do we know which way to increment?
+How do we know the start and end of the palindrome?
+bab
+aba
 
+brute force
+
+dict {
+    a: [leftmost, rightmost indices]
+    .....
+    z
+}
+
+heap (distance, "char", left, right)
+
+'''
+
+'''
+palindromic substring - if wrote in reverse order, it's the exact same string
+we can start at the beginning O(n^3)or...  
+start at the middle of the palindrome and work our way outwards
+consider each character as the middle => O(n^2)
+
+even and odd palindrome - consider this edge case
+
+time - O(n^2)
+space - O(n) for output str
 '''
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            # odd len palindromes
+            l, r = i, i # center position
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+            # even length
+            l, r = i, i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
         
+        return res
