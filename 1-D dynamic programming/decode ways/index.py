1027414033b558f944100eb95f3b171fba2bf2d1
goofy <goofydinh@gmail.com>
2022-04-23 15:33:19 -0700

    added failed atempt of decode ways

--- 1-D dynamic programming/decode ways/index.py
+++ 1-D dynamic programming/decode ways/index.py
@@ -1,4 +1,4 @@
-'''
+"""
 A message containing letters from A-Z can be encoded into numbers using the following mapping:
 
 'A' -> "1"
@@ -38,12 +38,74 @@ Constraints:
 
 1 <= s.length <= 100
 s contains only digits and may contain leading zero(s).
-'''
+"""
 
-'''
+"""
+the trick is 2 digit number, like J - 10
+06 is NOT F. F is 6.
+we have to find out how many ways it can be decoded
+12 => 12 or 1 and 2
+L or AB
 
-'''
+for 2 digits, the first digit can be 1 or 2
+if it's a 0, it can only be decoded 1 way
+there's no character for 0
+
+A 1
+B 2
+C 3
+D 4
+E 5
+F 6
+G 7
+H 8
+I 9
+J 10
+K 11
+L 12
+M 13
+N 14
+O 15
+P 16
+Q 17
+R 18
+S 19
+T 20
+U 21
+V 22
+W 23
+X 24
+Y 25
+Z 26
+
+10 1234
+11 23 
+J A B D
+J A W
+J L C D
+
+if it's 1 or 2 we gonna check the next index if exist. if it's any
+digits besides 0, we gonna add 1
+while iterating we don't add 0's
+"""
+
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
+        if s[0] == "0":
+            return 0
+        output = 1
+        for i in range(len(s)):            
+            if i == 0: # skip first
+                continue
+            if s[i] == "0" and s[i-1] not in ("1", "2"):
+                return 0
+            if s[i] != "0" and s[i-1] in ("1"):
+                output += 1
+            if s[i] not in ("0", "7", "8", "9") and s[i-1] in ("2"):
+                output += 1
+        return output
+
+sol = Solution()
+output = sol.numDecodings("12")
+print(output)
