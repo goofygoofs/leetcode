75d1dc32ba06d806c5d6beb985528caeb6607077
goofy <goofydinh@gmail.com>
2022-04-23 20:58:47 -0700

    added longest common sub using 2d dynamic programming. if text equal take 1 + the diagonal (i+1)(j+1) value. if not, take the max between bottom and right value

--- 2-D dynamic programming/longest common subsequence/index.py
+++ 2-D dynamic programming/longest common subsequence/index.py
@@ -32,9 +32,61 @@ text1 and text2 consist of only lowercase English characters.
 '''
 
 '''
+we can create a new substring from the original with deleting some characters without
+changing the relative order of the remaining charactreers
+match this substring with substring in text 2
 
+a   b   c   d   e
+1   0   1   0   1
+
+a   c   z   e
+1   1   0   1
+
+Q: how do we know whether we should delete or keep
+'''
+
+'''
+check the first letter of each text1 and text2. if they match then that's the longest common subsequence 
+then we can break it into a sub problem 
+1 + subproblem answer
+that's what dynamic programming is all about, finding the sub problem
+Q: what if the first text is not the same?
+then we cannot add 1 and find the longest common subsequence between the remainder of the two strings
+we can break it into a different subproblem. we can say it's possible the longest common subsequence could be between
+the two strings right 
+or between str1 move over by 1 /vice versa
+if they match we move diagonally . 
+if not match we check the right position and down posiiton
+Q what doe sthe right position mean? 
+it means we are taking the remaining substring from i and comparing it with substring of j+1 
+longest common between string and empty is 0 (bottom and right)
+going up means there was no match there. diagonal means there's match (+1)
+                j
+        a   c   e
+    a   3           0
+    b       2       0
+    c       2       0
+i   d           1   0
+    e           1   0
+        0   0   0   0
+bottom up dynamic programming 
+as we start from the bottom and work our way up how do we determine what values go into the cell?
+we are going to look at the characters and if they match each other
+we're gonna say 1 + value that's diagonal right
+if characters don't match, take the max of the bottom and right of the cell
+time - O(m*n) to go through both string lengths
+space - O(m*n) for the dp 2-d array
 '''
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) -1 , -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1] # add at diagonal
+                else: #characters don't mnatch
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j]) #don't match so we don't add 
         
+        return dp[0][0]
