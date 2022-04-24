fbd87c9e8fc6b4717e4f1903395966c7b85c45dc
goofy <goofydinh@gmail.com>
2022-04-24 14:39:28 -0700

    added  redo

--- toDo.py
+++ toDo.py
@@ -1,4 +1,17 @@
 '''
+couldn't solve (starting at coin change II)
+
+
+HARD (don't know if i can re-do it)
+    Coin Change II
+
+
+
+MEDIUM (can probably re-do it)
+
+
+
+EASY (can easily re-do it)
 
 
 '''
