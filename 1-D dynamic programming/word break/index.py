c36521b8c4cf5ac9ce0683d7a8f254770633c4bd
goofy <goofydinh@gmail.com>
2022-04-23 18:57:11 -0700

    added first failed attemp at word break

--- 1-D dynamic programming/word break/index.py
+++ 1-D dynamic programming/word break/index.py
@@ -32,12 +32,47 @@ All the strings of wordDict are unique.
 '''
 
 '''
-
+build a Trie
+if we reach trie.isWord = True, then dfs() into there
+dfs(s[0])
+leet ->T
+code ->
+cat(s)->
+ 
 '''
 
 from typing import List
 
+class Trie:
+    def __init__(self) -> None:
+        self.children = {}
+        self.isWord = False
+    
+    def add(self, word: str) -> None:
+        root = self.children
+        for char in word:
+            if char not in root:
+                root[char] = Trie()
+            root = root[char]
+        root.isWord = True
+
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # build Trie
+        self.trie = Trie()
+        for word in wordDict:
+            self.trie.add(word)
         
+        def helper(s: str, trie: Trie) -> bool:
+            if not s:
+                return True
+            char = s[0]
+            if trie.isWord:
+                return helper(s[1:], self.trie[char])
+            if char not in trie.children:
+                return False
+            if helper(s[1:], trie[char]):
+                return True
+
+        return helper(s, self.trie)
