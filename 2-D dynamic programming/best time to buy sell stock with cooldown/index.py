2bd439dffb8c543e0f63afff32bc5476afa7bd3a
goofy <goofydinh@gmail.com>
2022-04-24 12:59:30 -0700

    added maxProfit using dfs (i+1) for buy and (i+2) for sell+cooldownday and choice of cooldown i+1. key is (i, buying) buying is boolean whether able to buy, which changes when you just bought. value is profit and is max(buy, cooldown)

--- 2-D dynamic programming/best time to buy sell stock with cooldown/index.py
+++ 2-D dynamic programming/best time to buy sell stock with cooldown/index.py
@@ -26,6 +26,55 @@ Constraints:
 '''
 
 '''
+base case is if len(prices) < 2, return 0
+
+[1,2,3,0,2]
+|
+buy, sell, do nothing
+
+day 1
+do nothing - 0 
+buy - 1
+
+day 2
+do nothing - 0
+buy - 2
+sell - 1
+
+        1   2   3   0   2
+buy     1   2   3   0   2        
+sell        1   2   2   3
+nothing
+output = 0
+L R
+[1,2,3,0,2]
+ 0 
+ 
+ if nums[r] > nums[l]:
+     take profit
+Q: how do we know when to take profit and how do wse keep track if we don't?
+'''
+
+'''
+can't have multiple shares, have buy more than 1 share. 
+
+[1,2,3,0,2]
+            0
+        buy|            |cooldown
+          -1            0
+     sell| |cooldown  buy|   |cooldown
+        +1  -1
+        |cooldown
+    buy|(0+1)   |cooldown
+    |           +1
+    +1
+    .....
+O(2^n)
+use caching to O(n) time
+(i int, buy/sell bool)
+(n max, 2 max) => O(n)
+
+
 
 '''
 
@@ -34,4 +83,36 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # state: buying or selling?
+        #  if buy -> i + 1
+        #  if sell -> i + 2 (for cooldown day)
+
+        # cache
+        dp = {} # key=(i, buying) val=max_profit
         
+        # buying bool is "can i buy?" False if i already bought because
+        # can't buy more than 1 stock
+        def dfs(i, buying):
+            # print('i', i, 'buying', buying, 'dp', dp)
+            if i >= len(prices):
+                return 0
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+            
+            cooldown = dfs(i + 1, buying) # always have choice of cooldown
+            if buying:
+                buy = dfs(i + 1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
+            # print('final i', i, 'buying', buying, 'dp', dp)
+            # returns maximum value profit at index i (starting index will return max profit for entire array)
+            return dp[(i, buying)]
+        
+        return dfs(0, True)
+
+
+sol = Solution()
+output = sol.maxProfit([1,2,3,0,2])
+print(output)
