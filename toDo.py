776d748f62dab1da62673c31180e128094942ecd
goofy <goofydinh@gmail.com>
2022-06-08 16:21:30 -0700

    update

--- toDo.py
+++ toDo.py
@@ -1,17 +1,4 @@
 '''
-couldn't solve (starting at coin change II)
-
-
-HARD (don't know if i can re-do it)
-    Coin Change II
-
-
-
-MEDIUM (can probably re-do it)
-
-
-
-EASY (can easily re-do it)
 
 
 '''
